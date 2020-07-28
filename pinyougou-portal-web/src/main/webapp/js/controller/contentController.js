a133c4ddc7914a25275e0b6cef3190645c3266b3
cdtswdk <1445676933@qq.com>
2020-07-28 19:55:11 +0800

    第8天，广告管理与缓存[Redis]解决方案

---
+++ pinyougou-portal-web/src/main/webapp/js/controller/contentController.js
@@ -0,0 +1,17 @@
+/*****
+ * 定义一个控制层 controller
+ * 发送HTTP请求从后台获取数据
+ ****/
+app.controller("contentController",function($scope,$http,contentService){
+
+    $scope.contentList={};
+
+    //根据categoryid查询广告
+    $scope.getByCategoryId=function (categoryid) {
+        contentService.findByCategoryid(categoryid).success(function (response) {
+            //存储所有广告
+            $scope.contentList[categoryid]=response;
+        })
+    }
+
+});
